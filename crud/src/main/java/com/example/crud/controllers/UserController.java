55efb6c5e9c36486008d48f55cacb221258dbe8f
Mary2201 <maryc.mosso@gmail.com>
2024-09-30 14:40:37 -0500

    createcrud

--- crud/src/main/java/com/example/crud/controllers/UserController.java
+++ crud/src/main/java/com/example/crud/controllers/UserController.java
@@ -1,17 +1,13 @@
 package com.example.crud.controllers;
 
 import java.util.List;
-//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import com.example.crud.models.User;
-//import com.example.crud.repositorios.usuarioRepository;
 import com.example.crud.service.UserService;
 
 
